2b697d566d3ac707f680b61fa870f29e0b6fac01
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-22 15:04:19 +0000

    add views & urls for following pages in creator

--- quizulu_django_project/quiz_app/views.py
+++ quizulu_django_project/quiz_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Quiz
 from .forms import QuizForm
 
@@ -29,3 +30,23 @@ def quiz_creator(request):
 
 def success(request):
     return render(request, 'quiz_app/success.html')
+
+def multiplecreator(request):
+    return render(request, 'quiz_app/multiple-creator.html')
+
+def freecreator(request):
+    return render(request, 'quiz_app/free-creator.html')
+
+def picturecreator(request):
+    return render(request, 'quiz_app/picture-creator.html')
+
+def redirect_creator_mode(request):
+    if request.method == 'POST':
+        game_mode = request.POST.get('gamemode')
+        if game_mode == 'multipleChoice':
+            return redirect('quiz-multiple-creator')
+        elif game_mode == 'freeWriting':
+            return redirect('quiz-free-creator')
+        elif game_mode == 'withPictures':
+            return redirect('quiz-picture-creator')
+    return redirect('quiz-creator')
