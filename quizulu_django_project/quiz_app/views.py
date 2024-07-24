30688e807a5b63464161b4989ba7fa276ce24faa
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-24 19:18:11 +0000

    add multiple views for quiz creation process

--- quizulu_django_project/quiz_app/views.py
+++ quizulu_django_project/quiz_app/views.py
@@ -1,59 +1,68 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
 from .models import Quiz, Questions
 from .forms import QuizForm
-from django.views.generic.edit import CreateView
+import random
+
 
 def index(request):
     return render(request, 'quiz_app/index.html')
 
-def gamemodes(request):
-    return render(request, 'quiz_app/gamemodes.html')
-
-def multiplegamemode(request):
-    return render(request, 'quiz_app/multiple-categories.html')
-
-def freegamemode(request):
-    return render(request, 'quiz_app/free-categories.html')
-
-def picturegamemode(request):
-    return render(request, 'quiz_app/picture-categories.html')
-
+@login_required
 def quiz_creator(request):
     if request.method == 'POST':
         form = QuizForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('success')  # need to add the quiz list
+            quiz = form.save()
+            return redirect('question-creator', pk=quiz.pk)
     else:
         form = QuizForm()
     return render(request, 'quiz_app/quiz-creator.html', {'form': form})
 
 def success(request):
     return render(request, 'quiz_app/success.html')
-
-def multiplecreator(request):
-    return render(request, 'quiz_app/multiple-creator.html')
-
-def freecreator(request):
-    return render(request, 'quiz_app/free-creator.html')
-
-def picturecreator(request):
-    return render(request, 'quiz_app/picture-creator.html')
-
-def redirect_creator_mode(request):
-    if request.method == 'POST':
-        game_mode = request.POST.get('gamemode')
-        if game_mode == 'multipleChoice':
-            return redirect('quiz-multiple-creator')
-        elif game_mode == 'freeWriting':
-            return redirect('quiz-free-creator')
-        elif game_mode == 'withPictures':
-            return redirect('quiz-picture-creator')
-    return redirect('quiz-creator')
-
-
+        
 class QuestionCreateView(CreateView):
     model = Questions
-    fields = ['question']
-        
+    fields = ['question', 'correct_answer', 'answer1', 'answer2', 'answer3']
+    template_name = 'quiz_app/question_form.html'
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['quiz'] = get_object_or_404(Quiz, pk=self.kwargs['pk'])
+        return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        quiz = get_object_or_404(Quiz, pk=self.kwargs['pk'])
+        context['quiz'] = quiz
+        context['questions_count'] = quiz.questions_set.count()
+        return context
+
+    def form_valid(self, form):
+        form.instance.quiz_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('question-creator', kwargs={'pk': self.kwargs['pk']})
+    
+
+def list_categories(request):
+    categories = Quiz.objects.values_list('category', flat=True).distinct()
+    return render(request, 'quiz_app/categories.html', {'categories': categories})
+
+def random_quiz(request, category):
+    quizzes = Quiz.objects.filter(category=category)
+    if quizzes.exists():
+        quiz = random.choice(quizzes)
+        return redirect('quiz-detail', quiz_id=quiz.id)
+    else:
+        return render(request, 'quiz_app/no_quiz.html', {'category': category})
+    
+
+def quiz_detail(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    return render(request, 'quiz_app/quiz_detail.html', {'quiz': quiz})
