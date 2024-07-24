932be89e89b2a409d1a2ef7b358d07937a6c9c03
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-24 15:21:17 +0000

    edit url for questions

--- quizulu_django_project/quiz_app/urls.py
+++ quizulu_django_project/quiz_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='quiz-index'),
     path('gamemodes/', views.gamemodes, name='quiz-gamemodes'),
-    path('multiple-gamemode/', views.multiplegamemode, name='quiz-multiple-gamemode'),
+    path('multiple-gamemode/', views.QuestionCreateView.as_view(), name='quiz-multiple-gamemode'),
     path('free-gamemode/', views.freegamemode, name='quiz-free-gamemode'),
     path('picture-gamemode/', views.picturegamemode, name='quiz-picture-gamemode'),
     path('quiz-creator/', views.quiz_creator, name='quiz-creator'),
