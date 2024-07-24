9c676917376daf75ce7ceaf13dd4e53b59daf879
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-24 19:17:35 +0000

    delete old paths and create new for creation

--- quizulu_django_project/quiz_app/urls.py
+++ quizulu_django_project/quiz_app/urls.py
@@ -3,15 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='quiz-index'),
-    path('gamemodes/', views.gamemodes, name='quiz-gamemodes'),
-    path('multiple-gamemode/', views.QuestionCreateView.as_view(), name='quiz-multiple-gamemode'),
-    path('free-gamemode/', views.freegamemode, name='quiz-free-gamemode'),
-    path('picture-gamemode/', views.picturegamemode, name='quiz-picture-gamemode'),
+    path('categories/', views.list_categories, name='categories'),
     path('quiz-creator/', views.quiz_creator, name='quiz-creator'),
     path('success/', views.success, name='success'),
-    path('multiple-creator/', views.multiplecreator, name='quiz-multiple-creator'),
-    path('free-creator/', views.freecreator, name='quiz-free-creator'),
-    path('picture-creator/', views.picturecreator, name='quiz-picture-creator'),
-    path('redirect-creator-mode/', views.redirect_creator_mode, name='redirect-creator-mode'),
-    # Add more URL patterns here
+    path('quiz/<int:quiz_id>/', views.quiz_detail, name='quiz-detail'),
+    path('random_quiz/<str:category>/', views.random_quiz, name='random-quiz'),
+    path('quiz/<int:pk>/questions/new/', views.QuestionCreateView.as_view(), name='question-creator'),
+
 ]
+
