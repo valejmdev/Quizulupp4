f0c54fe5b6e3c968fe8bbc748229de430c33bcc9
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-25 16:23:01 +0000

    add views for user crud for quiz & play quiz views

--- quizulu_django_project/quiz_app/views.py
+++ quizulu_django_project/quiz_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from .models import Quiz, Questions
 from .forms import QuizForm
+import logging
 import random
 
 
@@ -50,6 +51,43 @@ class QuestionCreateView(CreateView):
         return reverse_lazy('question-creator', kwargs={'pk': self.kwargs['pk']})
     
 
+class QuizUpdateView(UpdateView):
+    model = Quiz
+    fields = ['title', 'category', 'description']
+    template_name = 'quiz_app/quiz_form.html'
+
+    def form_valid(self, form):
+        logging.info('Form is valid. Quiz is being updated.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        logging.error('Form is invalid. Errors: %s', form.errors)
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.pk})
+
+class QuizDeleteView(DeleteView):
+    model = Quiz
+    template_name = 'quiz_app/quiz_confirm_delete.html'
+    success_url = reverse_lazy('quiz-index')
+
+class QuestionUpdateView(UpdateView):
+    model = Questions
+    fields = ['question', 'correct_answer', 'answer1', 'answer2', 'answer3']
+    template_name = 'quiz_app/question_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.quiz.pk})
+
+class QuestionDeleteView(DeleteView):
+    model = Questions
+    template_name = 'quiz_app/question_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.quiz.pk})
+    
+
 def list_categories(request):
     categories = Quiz.objects.values_list('category', flat=True).distinct()
     return render(request, 'quiz_app/categories.html', {'categories': categories})
@@ -66,3 +104,42 @@ def random_quiz(request, category):
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     return render(request, 'quiz_app/quiz_detail.html', {'quiz': quiz})
+
+@login_required
+def my_quizzes(request):
+    quizzes = Quiz.objects.filter(created_by=request.user)
+    return render(request, 'quiz_app/my_quizzes.html', {'quizzes': quizzes})
+
+def list_categories(request):
+    categories = Quiz.objects.values_list('category', flat=True).distinct()
+    return render(request, 'quiz_app/categories.html', {'categories': categories})
+
+def random_quiz(request, category):
+    quizzes = Quiz.objects.filter(category=category)
+    if quizzes.exists():
+        quiz = random.choice(quizzes)
+        return redirect('quiz-detail', quiz_id=quiz.id)
+    else:
+        return render(request, 'quiz_app/no_quiz.html', {'category': category})
+
+def quiz_detail(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    return render(request, 'quiz_app/quiz_detail.html', {'quiz': quiz})
+
+def play_quiz(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    questions = quiz.questions_set.all()
+    current_question = 0
+    answers = []  # Define the answers variable here
+    if request.method == 'POST':
+        if 'next_question' in request.POST:
+            current_question += 1
+        elif 'submit_answer' in request.POST:
+            # Check the answer and update the user's score
+            pass
+            answers = [questions[current_question].correct_answer, questions[current_question].answer1, questions[current_question].answer2, questions[current_question].answer3]
+            random.shuffle(answers)
+    else:
+        answers = [questions[current_question].correct_answer, questions[current_question].answer1, questions[current_question].answer2, questions[current_question].answer3]
+        random.shuffle(answers)
+    return render(request, 'quiz_app/play_quiz.html', {'quiz': quiz, 'questions': questions, 'current_question': current_question, 'answers': answers})
