8a4c46825d1bc5df75ba28435182f9daee0414c5
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-26 17:13:29 +0000

    edit play view and create view, aswell login

--- quizulu_django_project/quiz_app/views.py
+++ quizulu_django_project/quiz_app/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Quiz, Question, UserQuizProgress
 from .forms import QuizForm
 import logging
 import random
-
 
 def index(request):
     return render(request, 'quiz_app/index.html')
@@ -23,7 +23,6 @@ def random_quiz(request, category):
         return redirect('quiz-detail', quiz_id=quiz.id)
     else:
         return render(request, 'quiz_app/no_quiz.html', {'category': category})
-    
 
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
@@ -35,23 +34,23 @@ def play_quiz(request, quiz_id):
     questions = list(quiz.question_set.all())
 
     if not questions:
-        return render(request, 'quiz_app/empty_quiz.html', {'quiz': quiz})  # Handle empty quiz scenario
+        return render(request, 'quiz_app/empty_quiz.html', {'quiz': quiz})
 
     progress, created = UserQuizProgress.objects.get_or_create(user=request.user, quiz=quiz)
     current_question_index = progress.current_question_index if not created else 0
-    
+
     if current_question_index >= len(questions):
         return redirect('quiz_results', quiz_id=quiz_id)
 
     current_question = questions[current_question_index]
 
     if request.method == 'POST':
-        if 'submit_answer' in request.POST:
-            selected_answer = request.POST.get('answer')
+        selected_answer = request.POST.get('answer')
+        
+        if selected_answer:
             if selected_answer == current_question.correct_answer:
                 progress.score += 1
 
-            # Move to next question
             progress.current_question_index += 1
 
             if progress.current_question_index >= len(questions):
@@ -61,7 +60,6 @@ def play_quiz(request, quiz_id):
                 progress.save()
                 return redirect('play_quiz', quiz_id=quiz_id)
 
-    # Gather answers
     answers = [current_question.correct_answer]
     if current_question.incorrect_answer1:
         answers.append(current_question.incorrect_answer1)
@@ -98,8 +96,7 @@ def my_quizzes(request):
     quizzes = Quiz.objects.filter(created_by=request.user)
     return render(request, 'quiz_app/my_quizzes.html', {'quizzes': quizzes})
 
-@login_required
-class QuizCreateView(CreateView):
+class MyQuizCreateView(LoginRequiredMixin, CreateView):
     model = Quiz
     form_class = QuizForm
     template_name = 'quiz_app/quiz_form.html'
@@ -111,8 +108,7 @@ class QuizCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('question-creator', kwargs={'pk': self.object.pk})
 
-@login_required
-class QuizUpdateView(UpdateView):
+class QuizUpdateView(LoginRequiredMixin, UpdateView):
     model = Quiz
     fields = ['title', 'category', 'description']
     template_name = 'quiz_app/quiz_form.html'
@@ -128,16 +124,12 @@ class QuizUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.pk})
 
-
-@login_required
-class QuizDeleteView(DeleteView):
+class QuizDeleteView(LoginRequiredMixin, DeleteView):
     model = Quiz
     template_name = 'quiz_app/quiz_confirm_delete.html'
     success_url = reverse_lazy('quiz-index')
 
-
-@login_required
-class QuestionUpdateView(UpdateView):
+class QuestionUpdateView(LoginRequiredMixin, UpdateView):
     model = Question
     fields = ['question', 'correct_answer', 'answer1', 'answer2', 'answer3']
     template_name = 'quiz_app/question_form.html'
@@ -145,17 +137,14 @@ class QuestionUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.quiz.pk})
 
-
-@login_required
-class QuestionDeleteView(DeleteView):
+class QuestionDeleteView(LoginRequiredMixin, DeleteView):
     model = Question
     template_name = 'quiz_app/question_confirm_delete.html'
 
     def get_success_url(self):
         return reverse_lazy('quiz-detail', kwargs={'quiz_id': self.object.quiz.pk})
 
-
-class QuestionCreateView(CreateView):
+class QuestionCreateView(LoginRequiredMixin, CreateView):
     model = Question
     fields = ['question_text', 'correct_answer', 'incorrect_answer1', 'incorrect_answer2', 'incorrect_answer3']
     template_name = 'quiz_app/question_form.html'
