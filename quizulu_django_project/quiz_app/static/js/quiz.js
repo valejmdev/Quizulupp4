ad0fe7cff4835b03e00054e6acdd7f5c51c4fcb3
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-26 19:15:20 +0000

    add even listener delete confirm &  generate link

--- quizulu_django_project/quiz_app/static/js/quiz.js
+++ quizulu_django_project/quiz_app/static/js/quiz.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Functionality for answer buttons
     const buttons = document.querySelectorAll('.answer-button');
     const hiddenInput = document.getElementById('selected-answer');
     const form = document.querySelector('form');
@@ -24,4 +25,40 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Please select an answer before submitting.');
         }
     });
+
+    // Functionality for delete confirmation
+    const deleteForms = document.querySelectorAll('.delete-form');
+
+    deleteForms.forEach(form => {
+        form.addEventListener('submit', function(event) {
+            if (!confirm('Are you sure you want to delete this quiz?')) {
+                event.preventDefault(); // Stop form submission
+            }
+        });
+    });
+
+    // Functionality for generating and copying quiz link
+    const linkModal = document.getElementById('linkModal');
+    const quizLinkInput = document.getElementById('quiz-link');
+    const copyLinkButton = document.getElementById('copy-link');
+
+    if (linkModal) {
+        console.log('1')
+        console.log('2')
+        const button = document.getElementById('modal-btn-test');
+        const quizId = button.getAttribute('data-quiz-id');
+        console.log('Quiz ID:', quizId); // Debugging line
+        const link = `${window.location.origin}/quiz/${quizId}/`;  // Adjust URL format as needed
+        console.log('Generated Link:', link); // Debugging line
+        quizLinkInput.value = link;
+        
+    }
+
+    if (copyLinkButton) {
+        copyLinkButton.addEventListener('click', function() {
+            quizLinkInput.select();
+            document.execCommand('copy');
+            alert('Link copied to clipboard!');
+        });
+    }
 });
