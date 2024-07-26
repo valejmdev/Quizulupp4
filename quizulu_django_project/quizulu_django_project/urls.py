78b747071ce3c82ac26b7d25a586863350dd8544
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-26 17:13:49 +0000

    delete whitespace

--- quizulu_django_project/quizulu_django_project/urls.py
+++ quizulu_django_project/quizulu_django_project/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confrim/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset'),
-     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset'),
     path('', include('quiz_app.urls')),
 ]
 
