6faf74b3aa5966071ddab0989126ba78695fa287
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-27 20:18:09 +0000

    add login view and edit profile view after bug

--- quizulu_django_project/users/views.py
+++ quizulu_django_project/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     if request.method == 'POST':
@@ -9,12 +11,11 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in with your credentials!')
-            return redirect('login')   
-    else: 
+            messages.success(request, f'Thank you for creating an account, {username}. You can now log in!')
+            return redirect('login')
+    else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
-
 
 @login_required
 def profile(request):
@@ -26,15 +27,26 @@ def profile(request):
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
-
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
-        'p_form': p_form 
-
+        'p_form': p_form
     }
 
     return render(request, 'users/profile.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('profile')  # or wherever you want to redirect after login
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
