197988ac899933164df1ccb8577b4597ffd6cd0c
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-28 19:07:36 +0000

    add whitenoise to settings and requirements

--- quizulu_django_project/quizulu_django_project/settings.py
+++ quizulu_django_project/quizulu_django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import mimetypes
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,8 +26,6 @@ SECRET_KEY = 'django-insecure-jjpa_g2!x811b&ontt+3g47vi3bizu%bn26*o9o_1st%9*j_6%
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-mimetypes.add_type("text/css", ".css", True)
 
 ALLOWED_HOSTS = [
     '8000-valejmdev-quizulupp4-297d69blsys.ws.codeinstitute-ide.net', 
@@ -52,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
