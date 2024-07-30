84db15ff82c58971934522ce2b2ae7c0db08229f
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-07-30 23:06:51 +0000

    add quiz_extras.py for css on form field

---
+++ quiz_app/templatetags/quiz_extras.py
@@ -0,0 +1,9 @@
+# quiz_app/templatetags/quiz_extras.py
+from django import template
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    """ Adds a CSS class to a form field """
+    return value.as_widget(attrs={'class': arg})
