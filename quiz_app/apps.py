cea71a84501a6f68476c47a691cb839d10562a84
Valentin Julius Muller <valentinjmueller055@gmail.com>
2024-08-01 00:12:27 +0000

    edit for pep8 conformity

--- quiz_app/apps.py
+++ quiz_app/apps.py
@@ -5,8 +5,8 @@ class QuizAppConfig(AppConfig):
     """
     Configuration class for the Quiz application.
 
-    This class is used to configure the 'quiz_app' Django application. 
-    It sets the default auto field type for models to 'BigAutoField', 
+    This class is used to configure the 'quiz_app' Django application.
+    It sets the default auto field type for models to 'BigAutoField',
     which is suitable for large datasets and provides a larger integer
     field for primary keys.
     """
